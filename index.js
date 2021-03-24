25efd39135fd00788918aa2865e777227b13abbf
Martin Kuba <mkuba@newrelic.com>
2021-03-24 15:55:49 -0700

    make examples exportable

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var schemas = require('./lib/schemas')
+var examples = require('./lib/examples')
 var encode = require('./lib/encoder')
 var decode = require('./lib/decoder')
 
 module.exports = {
   encode: encode,
   decode: decode,
-  schemas: schemas
+  schemas: schemas,
+  examples: examples
 }
