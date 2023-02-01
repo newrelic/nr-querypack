2fb0a726d4f59491d5d50e6c754e995b7db7d72b
Patrick Housley <patrickhousley@users.noreply.github.com>
2023-02-01 15:26:58 -0600

    [chore] Code formatting and dev-dependency updates (#5)

--- test/index.js
+++ test/index.js
@@ -1,32 +1,33 @@
-var test = require('tape')
-var querypack = require('../index')
-var testCases = require('../examples/all.json')
-var lintSchema = require('../lib/schema-linter')
+var test = require("tape");
+var querypack = require("../index");
+var testCases = require("../examples/all.json");
+var lintSchema = require("../lib/schema-linter");
 
-var schemaNames = Object.keys(querypack.schemas)
+var schemaNames = Object.keys(querypack.schemas);
 schemaNames.forEach(function (schemaName) {
-  test('validate schema ' + schemaName, function (t) {
-    var schema = querypack.schemas[schemaName]
-    var errors = lintSchema(schema)
-    t.deepEqual(errors, [], 'expected no schema validation errors')
-    t.end()
-  })
-})
+  test("validate schema " + schemaName, function (t) {
+    var schema = querypack.schemas[schemaName];
+    var errors = lintSchema(schema);
+    t.deepEqual(errors, [], "expected no schema validation errors");
+    t.end();
+  });
+});
 
 testCases.forEach(function (testCase) {
-  var schemaVersion = testCase.schema.name + '.' + testCase.schema.version + ' '
-  test('encoding ' + schemaVersion + testCase.name, function (t) {
-    var schema = testCase.schema
-    var roots = JSON.parse(testCase.json)
-    var result = querypack.encode(roots, schema)
-    t.equal(result, testCase.querypack, 'encoded output should match expected')
-    t.end()
-  })
+  var schemaVersion =
+    testCase.schema.name + "." + testCase.schema.version + " ";
+  test("encoding " + schemaVersion + testCase.name, function (t) {
+    var schema = testCase.schema;
+    var roots = JSON.parse(testCase.json);
+    var result = querypack.encode(roots, schema);
+    t.equal(result, testCase.querypack, "encoded output should match expected");
+    t.end();
+  });
 
-  test('decoding ' + schemaVersion + testCase.name, function (t) {
-    var result = querypack.decode(testCase.querypack)
-    var expected = JSON.parse(testCase.json)
-    t.deepEqual(result, expected, 'decoded output should match expected')
-    t.end()
-  })
-})
+  test("decoding " + schemaVersion + testCase.name, function (t) {
+    var result = querypack.decode(testCase.querypack);
+    var expected = JSON.parse(testCase.json);
+    t.deepEqual(result, expected, "decoded output should match expected");
+    t.end();
+  });
+});
